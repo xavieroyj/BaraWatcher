800e359d851f57292d78d90f7353823b279b5633
Xavier Ong <xavier.oyj1999@gmail.com>
2024-12-09 19:47:22 +0800

    Add google authentication

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,12 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
