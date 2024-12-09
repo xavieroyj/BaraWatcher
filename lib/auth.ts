42b2f2558fca288d852e640adb26be0c5102c941
Xavier Ong <xavier.oyj1999@gmail.com>
2024-12-09 18:44:39 +0800

    ABC

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,5 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
 import prisma from "./db";
  
 export const auth = betterAuth({
