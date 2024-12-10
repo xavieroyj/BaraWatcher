7cd1c377f1c79dff316a2008b95a4f904c2cc43b
Xavier Ong <xavier.oyj1999@gmail.com>
2024-12-10 09:10:12 +0800

    PLZ

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,8 +9,4 @@ export default async function Dashboard() {
       <ScamDetectionTable validationRequests={validationRequests} />
     </div>
   );
-}   <div>
-        <ScamDetectionTable validationRequests={validationRequests} />
-    </div>
-  );
 }
